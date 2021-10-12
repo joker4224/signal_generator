66eb34021826d03c8dd1062ee044bc84746f8e05
joker4224 <1659562651@qq.com>
2021-10-12 18:15:44 +0800

    现在可以显示频率

--- user/mian.c
+++ user/mian.c
@@ -1,15 +1,24 @@
 #include<reg52.h>
 #include "lcd1602.h"
-
+uchar f = 123;
 int main()
 {
 	LCD_Init();
 	LCD_Clear();
 	while(1)
 	{
-		LCD_Write_Com(0x80);
 		delay(5);
-		LCD_Write_Data(0x41);
+		LCD_Write_Com(0x80+4);
+		delay(5);
+		LCD_Write_Data( f / 100 + 0x30 );
+		delay(5);
+		LCD_Write_Data( f / 10 % 10 + 0x30 );
+		delay(5);
+		LCD_Write_Data( f % 10 + 0x30 );
+		delay(5);
+		LCD_Write_Data('H');
+		delay(5);
+		LCD_Write_Data('z');
 		delay(5);
 	}
 }
