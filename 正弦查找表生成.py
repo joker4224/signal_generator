6d248f0395a381e11a4906363e1f92718c10795e
joker4224 <1659562651@qq.com>
2021-10-13 16:25:31 +0800

    改为单定时器

--- 正弦查找表生成.py
+++ 正弦查找表生成.py
@@ -1,7 +1,7 @@
 import math
-lh=977
+lh=50
 sin_table=[]
-for i in range(0,1024,1) :
-	sin_i=math.sin(i/1024*2*math.pi)
-	sin_table.append(int((sin_i+1)*(lh-1)/2)+1)
+for i in range(0,1000,1) :
+	sin_i=math.sin(i/1000*2*math.pi)
+	sin_table.append(int((sin_i+1)*lh/2))
 print(sin_table)
