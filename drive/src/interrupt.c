4cef474a41ddb2cfb9b9c893d08f966c63467079
joker4224 <1659562651@qq.com>
2021-10-13 12:39:44 +0800

    新加一个按钮

--- drive/src/interrupt.c
+++ drive/src/interrupt.c
@@ -7,13 +7,26 @@ void interrupt_init()
 {
 	IT0=1;
 	EX0=1;
+	
+	IT1=1;
+	EX1=1;
+	
 	EA=1;
+	
 	PX0=1;
+	PX1=1;
 }
 
-void interrupt1() interrupt 0
+void interrupt0() interrupt 0
 {
 	f=f+1;
+	k=f;
+	show_freq(f);
+}
+
+void interrupt1() interrupt 2
+{
+	f=f-1;
 	k=f;
 	show_freq(f);
 }
